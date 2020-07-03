8f7881537c3dce5fb016ec4a776d2263b027202b
Kondin Dmitriy <dkondin@gmail.com>
2020-07-03 11:55:31 +0700

    убираем лишнее

--- src/routes/entity.php
+++ src/routes/entity.php
@@ -1,15 +1,5 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Backpack\NewsCRUD Routes
-|--------------------------------------------------------------------------
-|
-| This file is where you may define all of the routes that are
-| handled by the Backpack\NewsCRUD package.
-|
-*/
-
 Route::group([
     'namespace' => 'Sitebill\Entity\app\Http\Controllers\Admin',
     'prefix' => config('backpack.base.route_prefix', 'admin'),
